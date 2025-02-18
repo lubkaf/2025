219052575675d67c5092e1d971dd31090ad32f8b
Filip Lubka <filip.lubka2007@gmail.com>
2025-02-18 21:51:21 +0100

    basic knowledge about events in c#

---
+++ C#/events/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace events
+{
+    public delegate void EventHandler(string message);
+
+    public class Sender
+    {
+        public event EventHandler EventOccured;
+        public void TriggerEvent(string message)
+        {
+            EventOccured?.Invoke(message);
+        }
+    }
+
+    public class Receiver
+    {
+        public void ReactToEvent(string message){
+            File.AppendAllText("file.txt", $"{DateTime.Now} Message received: {message}\n");
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Sender sender = new Sender();
+            Receiver receiver = new Receiver();
+
+            sender.EventOccured += receiver.ReactToEvent;
+
+            sender.TriggerEvent("Hello world!");
+        }
+    }
+}
